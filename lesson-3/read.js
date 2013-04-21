50ae3458556023519fa8fc3b794d01c02755bbc5
Lucas <metallucas.ds@gmail.com>
2013-04-21 13:21:19 -0300

    Finish for today

--- lesson-3/read.js
+++ lesson-3/read.js
@@ -1,6 +1,12 @@
+// var fs = require("fs");
+// console.log("Starting");
+// fs.readFile("../lesson-1/sample.txt", function(error, data) {
+// 	console.log("Contents: " + data);
+// });
+// console.log("Carry on executing");
+
 var fs = require("fs");
 console.log("Starting");
-fs.readFile("../lesson-1/sample.txt", function(error, data) {
-	console.log("Contents: " + data);
-});
+var content = fs.readFileSync("../lesson-1/sample.txt");
+console.log("Contents: " + content);
 console.log("Carry on executing");
